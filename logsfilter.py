e644f5d862bee374f421b54c410c35b619d76fcf
Carlo Palmieri <cpalmieri@redhat.com>
2020-08-06 17:11:29 +0200

    added short options

--- logsfilter.py
+++ logsfilter.py
@@ -11,11 +11,11 @@ class DateValidator(argparse.Action):
 
 parser = argparse.ArgumentParser(
     description='Sort and filter an unsorted APICAST log file')
-parser.add_argument('--input', type=str, help='Input file', required=True)
-parser.add_argument('--output', type=str, help='Output file', default='output.log')
-parser.add_argument('--start', type=str, help='The start of the interval to '
+parser.add_argument('-i', '--input', type=str, help='Input file', required=True)
+parser.add_argument('-o', '--output', type=str, help='Output file', default='output.log')
+parser.add_argument('-s', '--start', type=str, help='The start of the interval to '
     'extract in the format %%Y/%%m/%%d %%H:%%M:%%S e.g. 2020/01/08 08:30:05', action=DateValidator)
-parser.add_argument('--end', type=str, help='The end of the interval to '
+parser.add_argument('-e', '--end', type=str, help='The end of the interval to '
     'extract in the format %%Y/%%m/%%d %%H:%%M:%%S e.g. 2020/01/09 08:30:05', action=DateValidator)
 
 args = parser.parse_args()
